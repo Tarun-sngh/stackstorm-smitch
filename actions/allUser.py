676175895d2496a9278b19170dd92815853d7246
Tarun-sngh <tarunsingh2118@gmail.com>
2021-08-18 00:53:07 +0530

    auth

--- actions/allUser.py
+++ actions/allUser.py
@@ -7,9 +7,9 @@ from st2common.runners.base_action import Action
 
 class SmitchGetUsers(Action):
     def run(self, xApiKey):
-        auth_string = f"x-api-key:{xApiKey}"
-        header= {'headers' : f"{auth_string}"}
+
+        header = {'x-api-key' : xApiKey}
         url = 'https://app.api.developer.mysmitch.com/​v1​/app​/users'
 
-        users = requests.get(url, auth = header)
+        users = requests.get(url, headers = header)
         return(True,users)
