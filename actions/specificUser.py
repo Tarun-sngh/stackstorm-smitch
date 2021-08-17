85ef7ef3deec14fe7ee3eaa8fd08fed31b018f31
Tarun-sngh <tarunsingh2118@gmail.com>
2021-08-18 00:10:17 +0530

    auth

--- actions/specificUser.py
+++ actions/specificUser.py
@@ -8,12 +8,8 @@ from st2common.runners.base_action import Action
 class SmitchGetUser(Action):
     def run(self, xApiKey, userId):
 
-        auth = {"headers": { "x-api-key": xApiKey } }
+        auth = {'headers': { 'x-api-key': xApiKey }}
         url = f"https://app.api.developer.mysmitch.com/​v1​/app​/user?user_id={userId}"
 
         user = requests.get(url, auth = auth)
-<<<<<<< HEAD
         return(True)
-=======
-        return(True)
->>>>>>> 5d8096effd43d1a426ee656b55cf14da47eef79d
